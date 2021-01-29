072a7d060d7b33a9e3cc678c0af0a594d4b06343
Mansi <ma.chavan1999@gmail.com>
2021-01-29 22:02:49 +0530

    Maven Project for HelloWorld

---
+++ src/test/java/com/ex/hellotest/HelloWorldTest.java
@@ -0,0 +1,20 @@
+package com.ex.hellotest;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class HelloWorldTest extends TestCase {
+
+	public HelloWorldTest(String testName) {
+		super(testName);
+	}
+
+	public static Test suite() {
+		return new TestSuite(HelloWorldTest.class);
+	}
+
+	public void testApp() {
+		assertTrue(true);
+	}
+}
